03c4c1477928c2b9184639a1860cb608f49593c5
Maciej Król <maciekgplay@gmail.com>
2024-09-12 16:51:29 +0200

    fix: homepage overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Container from '@/components/container'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
+// Height of the header + <main> element padding
+const HEIGHT_OFFSET = 132
+
 export default function Home() {
   return (
     <Container>
-      <div className="flex flex-col grow justify-center items-center gap-4 min-h-[calc(100vh-68px)] text-center">
+      <div
+        className={`flex flex-col grow justify-center items-center gap-4 min-h-[calc(100vh-${HEIGHT_OFFSET}px)] text-center`}
+      >
         <h1 className="text-4xl xl:text-5xl font-bold">WooCommerce as Headless CMS</h1>
         <p className="text-lg xl:text-xl max-w-xl">
           This is a test project which tries to use WooCommerce as a headless CMS for a
